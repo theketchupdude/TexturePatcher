15dec6e8aedc26857df27cf3705e36acf970fb96
theketchupdude <tom@ascholer.org>
2018-06-18 08:35:29 -0700

    Worked on restart cycle and added new class

---
+++ src/io/github/theketchupdude/TexturePack.java
@@ -0,0 +1,5 @@
+package io.github.theketchupdude;
+
+public class TexturePack {
+
+}
--- src/io/github/theketchupdude/TexturePatcher.java
+++ src/io/github/theketchupdude/TexturePatcher.java
@@ -3,6 +3,7 @@ package io.github.theketchupdude;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
@@ -12,6 +13,9 @@ import java.util.logging.Logger;
 
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.dropbox.core.DbxException;
@@ -19,14 +23,20 @@ import com.dropbox.core.v2.files.GetTemporaryLinkErrorException;
 
 public class TexturePatcher extends JavaPlugin {
 	
+	int restarted = 0;
+	
+	boolean firstLoad = false;
+	
 	static Logger log = Logger.getLogger("Minecraft");
 	
 	File data = new File(this.getDataFolder() + "/texturepatcher.dat");
+	FileOutputStream dataWriter;
+	FileInputStream dataReader;
 	File temp = new File(this.getDataFolder() + "/tmp/");
 	
 	DropboxLink link = new DropboxLink(this.getConfig().getString("DropboxApiKey"));
 	
-	static File serverproperties = new File(System.getProperty("user.dir") + "/server.properties");
+	static File serverProperties = new File(System.getProperty("user.dir") + "/server.properties");
 	
 	public void onLoad(){
 		log.info("[TexturePatcher] Loaded.");
@@ -36,6 +46,24 @@ public class TexturePatcher extends JavaPlugin {
 			try {
 				data.createNewFile();
 				this.saveDefaultConfig();
+				firstLoad = true;
+			} catch (IOException e) {
+				log.severe(e.getLocalizedMessage());
+			}
+		}
+		
+		//Initialize data reading and writing
+		try {
+			dataWriter = new FileOutputStream(data);
+			dataReader = new FileInputStream(data);
+		} catch (FileNotFoundException e) {
+			log.severe(e.getLocalizedMessage());
+		}
+		
+		if(firstLoad){
+			try {
+				dataWriter.write(0);
+				dataWriter.close();
 			} catch (IOException e) {
 				log.severe(e.getLocalizedMessage());
 			}
@@ -44,6 +72,28 @@ public class TexturePatcher extends JavaPlugin {
 	
 	public void onEnable(){
 		log.info("[TexturePatcher] Enabled.");
+		
+		try {
+			restarted = dataReader.read();
+			dataReader.close();
+		} catch (IOException e) {
+			log.severe(e.getLocalizedMessage());
+		}
+		
+		//Finds out if server restart has happened already
+		if(restarted == 1){
+			//Properties are updated - set to restart
+			try {
+				dataWriter.write(0);
+				dataWriter.close();
+			} catch (IOException e) {
+				log.severe(e.getLocalizedMessage());
+			}
+		}else{
+			//Do updating and then restart
+			//updateProperties(serverProperties);
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "restart");
+		}
 	}
 	
 	public void onDisable(){
@@ -68,6 +118,7 @@ public class TexturePatcher extends JavaPlugin {
 		}
 	}
 	
+	//Updates the server.properties file
 	private static void updateProperties(File propertyFile, File texturepack, DropboxLink link) throws IOException, NoSuchAlgorithmException, GetTemporaryLinkErrorException, DbxException{
 		
 		Properties properties = new Properties();
@@ -77,6 +128,6 @@ public class TexturePatcher extends JavaPlugin {
 		properties.load(stream);
 		
 		properties.put("resource-pack-sha1", calcSHA1(texturepack));
-		properties.put("resource-pack", link.client.files().getTemporaryLink("/"));
+		properties.put("resource-pack", link.client.files().getTemporaryLink("/pack.zip"));
 	}
 }

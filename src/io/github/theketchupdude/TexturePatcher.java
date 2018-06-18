84e754465b2aaf5d7c678d9ca19772102cfe11f7
theketchupdude <tom@ascholer.org>
2018-06-18 09:16:35 -0700

    Deleted unneeded code and added event handler

--- src/io/github/theketchupdude/TexturePatcher.java
+++ src/io/github/theketchupdude/TexturePatcher.java
@@ -5,27 +5,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Properties;
 import java.util.logging.Logger;
 
-import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.dropbox.core.DbxException;
-import com.dropbox.core.v2.files.GetTemporaryLinkErrorException;
-
-public class TexturePatcher extends JavaPlugin {
-	
-	int restarted = 0;
-	
-	boolean firstLoad = false;
+public class TexturePatcher extends JavaPlugin {	
 	
 	static Logger log = Logger.getLogger("Minecraft");
 	
@@ -46,7 +32,6 @@ public class TexturePatcher extends JavaPlugin {
 			try {
 				data.createNewFile();
 				this.saveDefaultConfig();
-				firstLoad = true;
 			} catch (IOException e) {
 				log.severe(e.getLocalizedMessage());
 			}
@@ -59,75 +44,17 @@ public class TexturePatcher extends JavaPlugin {
 		} catch (FileNotFoundException e) {
 			log.severe(e.getLocalizedMessage());
 		}
-		
-		if(firstLoad){
-			try {
-				dataWriter.write(0);
-				dataWriter.close();
-			} catch (IOException e) {
-				log.severe(e.getLocalizedMessage());
-			}
-		}
 	}
 	
 	public void onEnable(){
 		log.info("[TexturePatcher] Enabled.");
-		
-		try {
-			restarted = dataReader.read();
-			dataReader.close();
-		} catch (IOException e) {
-			log.severe(e.getLocalizedMessage());
-		}
-		
-		//Finds out if server restart has happened already
-		if(restarted == 1){
-			//Properties are updated - set to restart
-			try {
-				dataWriter.write(0);
-				dataWriter.close();
-			} catch (IOException e) {
-				log.severe(e.getLocalizedMessage());
-			}
-		}else{
-			//Do updating and then restart
-			//updateProperties(serverProperties);
-			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "restart");
-		}
 	}
 	
 	public void onDisable(){
 		log.info("[TexturePatcher] Disabled.");
-		
 	}
 	
-	private static String calcSHA1(File file) throws FileNotFoundException, IOException, NoSuchAlgorithmException {
-
-		MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-		try (InputStream input = new FileInputStream(file)) {
-
-			byte[] buffer = new byte[8192];
-			int len = input.read(buffer);
-
-			while (len != -1) {
-				sha1.update(buffer, 0, len);
-				len = input.read(buffer);
-			}
-
-			return new HexBinaryAdapter().marshal(sha1.digest());
-		}
-	}
-	
-	//Updates the server.properties file
-	private static void updateProperties(File propertyFile, File texturepack, DropboxLink link) throws IOException, NoSuchAlgorithmException, GetTemporaryLinkErrorException, DbxException{
-		
-		Properties properties = new Properties();
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		InputStream stream = loader.getResourceAsStream(propertyFile.getAbsolutePath());
-		
-		properties.load(stream);
-		
-		properties.put("resource-pack-sha1", calcSHA1(texturepack));
-		properties.put("resource-pack", link.client.files().getTemporaryLink("/pack.zip"));
-	}
+	@EventHandler
+	public void onPlayerJoinWorld(PlayerJoinEvent e){}
 }
+	
